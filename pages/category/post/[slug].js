36e08e5e25fb984da84c68e69b7daa05d282916a
*ladeed <muhammed.ladeed123@gmail.com>
2021-11-08 21:57:53 +0530

    second commit

---
+++ pages/category/post/[slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Slug= () => {
+    return (
+        <div className="flex justify-center align-center">
+            <h2 className="font-semibold"> Plaese refer the Category from the carousel to continue your reading </h2>
+        </div>
+    )
+}
+
+export default Slug
